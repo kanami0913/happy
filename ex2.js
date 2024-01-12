1d3914a9b55b818872d2da3082d5b522f043e45b
kanami0913 <131091876+kanami0913@users.noreply.github.com>
2024-01-12 21:24:48 +0900

    Update ex2.js

--- ex2.js
+++ ex2.js
@@ -1,2 +1,2 @@
-function imgchange3() {document.getElementById('san').src = './image/3.jpg';}
-function imgchange4() {document.getElementById('yon').src = './image/4.jpg';}
+function imgchange3() {document.getElementById('san').src = './image/4.jpg';}
+function imgchange4() {document.getElementById('yon').src = './image/3.jpg';}
