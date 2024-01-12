b775020a69a3dd57121d5273146d29dc73fbde83
kanami0913 <131091876+kanami0913@users.noreply.github.com>
2024-01-12 21:43:43 +0900

    Update ex1.js

--- ex1.js
+++ ex1.js
@@ -1,3 +1,3 @@
-function imgchange1() {document.getElementById('miki').src = './image/mickeymouse.jpg';}
-function imgchange2() {document.getElementById('miki').src = './image/2.jpg';}
+function imgchange1() {document.getElementById('m').src = './image/mickeymouse.jpg';}
+function imgchange2() {document.getElementById('m').src = './image/2.jpg';}
 
